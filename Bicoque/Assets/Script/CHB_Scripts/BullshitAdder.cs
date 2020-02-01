862ba84aa8a30c2f902c3b1d59a9da9eae6b5648
Charles Bentein <c.bentein@rubika-edu.com>
2020-02-01 19:59:03 +0100

    UI d'inventaire ready

---
+++ Bicoque/Assets/Script/CHB_Scripts/BullshitAdder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BullshitAdder : MonoBehaviour
+{
+    Inventory inventory;
+    void Start()
+    {
+        inventory = GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            inventory.AddIron(2);
+            inventory.AddPlanks(2);
+            inventory.AddBricks(2);
+        }
+    }
+}
--- Bicoque/Assets/Script/CHB_Scripts/Inventory.cs
+++ Bicoque/Assets/Script/CHB_Scripts/Inventory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class Inventory : MonoBehaviour
 
     //Parts
     private int pt_planks;
-    private int pt_rivets;
+    private int pt_bars;
     private int pt_bricks;
     private int pt_pebbles;
 
@@ -19,7 +20,27 @@ public class Inventory : MonoBehaviour
     private int tr_wolftrap;
     private int tr_barricade;
     private int tr_sandbag;
-    
+
+    [SerializeField]
+    private GameObject woodCount;
+    [SerializeField]
+    private GameObject stoneCount;
+    [SerializeField]
+    private GameObject ironCount;
+    [SerializeField]
+    private GameObject plankCount;
+    [SerializeField]
+    private GameObject brickCount;
+    [SerializeField]
+    private GameObject barCount;
+
+    private Text woodText;
+    private Text ironText;
+    private Text stoneText;
+    private Text plankText;
+    private Text brickText;
+    private Text barText;
+
     private void Start()    /* Init at beginning of run */
     {
         mt_wood = 0;
@@ -27,13 +48,30 @@ public class Inventory : MonoBehaviour
         mt_iron = 0;
 
         pt_planks = 0;
-        pt_rivets = 0;
+        pt_bars = 0;
         pt_bricks = 0;
         pt_pebbles = 0;
 
         tr_wolftrap = 0;
         tr_barricade = 0;
         tr_sandbag = 0;
+
+        woodText = woodCount.GetComponent<Text>();
+        ironText = ironCount.GetComponent<Text>();
+        stoneText = stoneCount.GetComponent<Text>();
+        plankText = plankCount.GetComponent<Text>();
+        brickText = brickCount.GetComponent<Text>();
+        barText = barCount.GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        woodText.text = mt_wood.ToString();
+        stoneText.text = mt_stone.ToString();
+        ironText.text = mt_iron.ToString();
+        plankText.text = pt_planks.ToString();
+        brickText.text = pt_bricks.ToString();
+        barText.text = pt_bars.ToString();
     }
 
     //Increment materials
@@ -78,14 +116,14 @@ public class Inventory : MonoBehaviour
         pt_planks -= consume;
     }
 
-    public void AddRivets(int get)
+    public void AddBars(int get)
     {
-        pt_rivets += get;
+        pt_bars += get;
     }
 
-    public void UseRivets(int consume)
+    public void UseBars(int consume)
     {
-        pt_rivets -= consume;
+        pt_bars -= consume;
     }
 
     public void AddBricks(int get)

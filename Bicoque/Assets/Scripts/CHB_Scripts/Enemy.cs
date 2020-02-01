3d3ab15698c0b1315366dd36f39a00ee73312f1c
Charles Bentein <c.bentein@rubika-edu.com>
2020-02-01 16:07:43 +0100

    C'est un ennemi qui suit le joueur

---
+++ Bicoque/Assets/Scripts/CHB_Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector2 direction;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        direction = (player.transform.position - transform.position).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+    }
+
+    public void Follow()
+    {
+        direction = (player.transform.position - transform.position).normalized;
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+}

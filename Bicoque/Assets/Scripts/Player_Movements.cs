89036aaef48189b3420a8bfb88506fd8bef76533
Lougma <l.vitrant@rubika-edu.com>
2020-02-01 22:26:15 +0100

    Méca Ramassage terminée

--- Bicoque/Assets/Scripts/Player_Movements.cs
+++ Bicoque/Assets/Scripts/Player_Movements.cs
@@ -8,10 +8,23 @@ public class Player_Movements : MonoBehaviour
     Transform player;
     public float jumpForce = 10;
     public float speed = 5;
+    public float distanceDetection = 1;
+    public bool playerIsOnGround = true;
     private bool inControl = true;
 
     private void FixedUpdate()
     {
+        /*RaycastHit2D hit = Physics2D.Raycast(player.position, Vector2.down, distanceDetection, LayerMask.GetMask("Ground"));
+
+        if (hit)
+        {
+            playerIsOnGround = true;
+        }
+        else
+        {
+            playerIsOnGround = false;
+        }*/
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.velocity = new Vector2(-speed , rb2d.velocity.y);
@@ -33,6 +46,13 @@ public class Player_Movements : MonoBehaviour
         if (!inControl)
         {
             rb2d.velocity = new Vector2(0, transform.position.y);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Item"))
+        {
+            Destroy(collider.gameObject);
         }
     }
 }

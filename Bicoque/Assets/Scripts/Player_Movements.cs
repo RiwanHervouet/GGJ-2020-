98e457486566fd7ebc9e3333e415db75379a1b45
Lougma <l.vitrant@rubika-edu.com>
2020-02-01 14:35:44 +0100

    Déplacements terminés

---
+++ Bicoque/Assets/Scripts/Player_Movements.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movements : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    Transform player;
+    public float jumpForce = 10;
+    public float speed = 5;
+    private bool inControl = true;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.velocity = new Vector2(-speed , rb2d.velocity.y);
+            inControl = true;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.velocity = new Vector2(speed , rb2d.velocity.y);
+            inControl = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+            inControl = true;
+        }
+
+        if (!inControl)
+        {
+            rb2d.velocity = new Vector2(0, transform.position.y);
+        }
+    }
+}
